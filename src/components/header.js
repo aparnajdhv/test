7f8473ab076f3d7eea6edf5c94978e0ff0b1c2fa
Aparna Kadam <aparna.jdhv@gmail.com>
2023-04-29 10:59:47 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,15 @@ import React from 'react'
 
  const header = () => {
   return (
-    <div className='bg-primary text-center text-white header'>Header</div>
+    <div className='header'>
+      <div className='logo' ><i class='fas fa-dove' style='font-size:36px'></i></div>
+      <div className='signInUp'>
+          <a className='signUp '>Sign Up</a>
+          <a className='login '>Sign In</a>
+          
+          
+      </div>
+    </div>
   )
 }
 export default header
