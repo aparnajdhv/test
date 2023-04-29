38b24c8ce19466e3946f0660c3548a9187bd63e1
Aparna Kadam <aparna.jdhv@gmail.com>
2023-04-29 11:07:52 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
  const header = () => {
   return (
     <div className='header'>
-      <div className='logo' ><i class='fas fa-dove' style='font-size:36px'></i></div>
+      <div className='logo' >Logo</div>
       <div className='signInUp'>
           <a className='signUp '>Sign Up</a>
           <a className='login '>Sign In</a>
