7ca2a9103b66501ca11800c3dcf4d4a3047f827e
Aparna Kadam <aparna.jdhv@gmail.com>
2023-05-02 11:22:53 +0530

    commit message

--- src/components/menu.js
+++ src/components/menu.js
@@ -18,9 +18,9 @@ export const Menu = () => {
   return (
     <div className='menu'>
         <ul>
-            <li><Link id='home' className={menuItem=='home' && 'activeMenu'} onClick={fnClick} href='/home'>Home</Link></li>
-            <li><Link id='about' className={menuItem=='about' && 'activeMenu'} onClick={fnClick} href='/about'>About</Link></li>
-            <li><Link id='contact' className={menuItem=='contact' && 'activeMenu'} onClick={fnClick} href='/contact'>Contact</Link></li>
+            <li><Link id='home' className={menuItem=='home' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/home'>Home</Link></li>
+            <li><Link id='about' className={menuItem=='about' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/about'>About</Link></li>
+            <li><Link id='contact' className={menuItem=='contact' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/contact'>Contact</Link></li>
         </ul>
     </div>
   )
