08311799b7d3d61be52d9e0f7a67cb4ca3af5d5a
Aparna Kadam <aparna.jdhv@gmail.com>
2023-05-04 10:53:56 +0530

    mobile menu updated

--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,13 @@ import React from 'react'
       <div className='svg'>
       {/* <svg  viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,149.3C384,128,480,64,576,80C672,96,768,192,864,213.3C960,235,1056,181,1152,154.7C1248,128,1344,128,1392,128L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg> */}
       </div>
-      <div className='logo' >Logo</div>
-      <div className='signInUp'>
+      <div className='logo' >
+      <img className="logo-img" src='images/logo.png' alt="logo"/>
+      </div>
+      {/* <div className='signInUp'>
           <a className='login '>Login</a> 
           <a className='signUp '>Sign Up</a>
-                  
-          
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,21 +3,61 @@ import React, { useEffect, useState } from 'react'
 
 
 export const Menu = () => {
-  const [menuItem,setmenuItem]=React.useState('');
-  
-  const fnClick=(eve)=>{
-    setmenuItem(eve.target.id);
+  const [menuItem,setMenuItem]=useState('')
+  const [isMobileMenu,setIsMobileMenu]=useState(false)
+  const [right,setRight]=useState(-150)
 
+  //mobile menu
+  const getBrowserWidth=()=>{
+    let timeoutId;
+    let flag=true
+    setIsMobileMenu(document.body.offsetWidth <600 ? true : false)
+    window.addEventListener('resize',()=>{
+      let width=document.body.offsetWidth
+    
+      if(flag){
+        flag=false;
+        setIsMobileMenu(width <600 ? true : false)
+      }
+      clearTimeout(timeoutId)
+      timeoutId=setTimeout(()=>{
+        setIsMobileMenu(width <600 ? true : false)
+      },500)
+   
+    })
   }
 
+  
   useEffect(()=>{
-    let pathName=window.location.pathname;
-    setmenuItem(pathName.slice(1)|| 'home')
+    //menu
+      let pathName=window.location.pathname
+      setMenuItem(pathName.slice(1) || 'home')
+      //mobile-menu
+      getBrowserWidth()
   },[])
+  
+  const fnClick=(eve)=>{
+    //menu
+       setMenuItem(eve.target.id)
+
+       //mobile menu
+       if(isMobileMenu){
+        setRight(-150)
+       }
+  }
+  const fnMobileMenuBtnClick=()=>{
+    setRight(right === 0 ? -150 : 0)
+  }
+  const fnClose=()=>{
+    setRight(-150)
+  }
 
   return (
     <div className='menu'>
-        <ul>
+      {isMobileMenu && <button onClick={fnMobileMenuBtnClick} className='mobile-menu-btn'><i class="fa fa-bars"></i></button>}
+      
+      <ul style={{right:right}} className={isMobileMenu ? 'mobile-menu' : 'menu'}>
+      {isMobileMenu && <span onClick={fnClose} className='close-btn'>x</span>}
             <li><Link id='home' className={menuItem=='home' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/home'>Home</Link></li>
             <li><Link id='about' className={menuItem=='about' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/about'>About</Link></li>
             <li><Link id='contact' className={menuItem=='contact' && 'activeMenu' || 'menuList'} onClick={fnClick} href='/contact'>Contact</Link></li>
